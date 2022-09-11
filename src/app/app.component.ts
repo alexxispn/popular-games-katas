21be75c0ec01c38a780fef6a9c0d3dc9f18bb93d
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2022-09-11 19:15:06 +0100

    html & css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Word} from "./interfaces/word";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'popular-games-katas';
+  clue: string = '';
+  solution: string[] = [];
+  word:Word = {
+    name: "perro",
+    clue: "El mejor amigo del hombre"
+  }
+
+  ngOnInit() {
+    console.log(this.clue);
+    }
 }
